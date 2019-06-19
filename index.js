0fe72e2cc51939a7f3e2e6598b59c799037bd584
droll-accelerator-1483 <srsani@gmail.com>
2019-06-19 13:03:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo (name) {
   return `Happy holidays, ${name}!`
   }
   
-function holidayCountdown (holiday,, days) {
+function holidayCountdown (holiday, days) {
   return `It's ${days} until ${holiday}!`
   
 }
